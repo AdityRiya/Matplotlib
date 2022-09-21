84df6a1e2fd2c7139fde219b8f79ef53a76763b4
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-09-22 00:05:33 +0600

    5th and 6th code done

---
+++ matplotlib5.py
@@ -0,0 +1,15 @@
+import numpy as np
+from matplotlib import pyplot as plt
+x = np.arange(1,11)
+print(x)
+y1 = 2*x
+y2 = (x-1)
+print(y1)
+print(y2)
+plt.title("Two Line Plot using Matplatlib")
+plt.xlabel('x axies')
+plt.ylabel('y axies')
+plt.plot(x,y1,color='g', linestyle='-',linewidth=1)
+plt.plot(x,y2,color='r', linestyle='-',linewidth=1)
+plt.grid(True)
+plt.show()
